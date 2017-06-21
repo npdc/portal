cac6a7525e187ae16df2ba1e2d67c4973b53fdb0
Marten Tacoma <Marten.Tacoma@nioz.nl>
2017-06-21 10:34:08 +0200

    dissalow linebreaks in phone numbers on person report

--- app/npdc/template/person_report.tpl.php
+++ app/npdc/template/person_report.tpl.php
@@ -10,10 +10,10 @@ echo $this->data['mail'];
 $fields = ['phone_personal'=>'Direct','phone_secretariat'=>'General','phone_mobile'=>'Mobile'];
 foreach($fields as $id=>$label){
 	if(!empty($this->data[$id])){
-		echo ' |&nbsp;'.$this->data[$id].' ('.$label.')';
+		echo ' <nobr>| '.$this->data[$id].' ('.$label.')</nobr>';
 	}
 }
-echo ' |&nbsp;'.$perms['name'].'</p>';
+echo ' <nobr>| '.$perms['name'].'</nobr></p>';
 
 ?>
 <button onclick="$('.hiddenSubDiv').slideDown();$('.collapsible').removeClass('hidden');">Expand all</button> <button onclick="$('.hiddenSubDiv').slideUp();$('.collapsible').addClass('hidden');">Collapse all</button>
