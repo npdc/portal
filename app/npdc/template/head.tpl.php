0a8ad433d438c7c935dac291966989b6ca1ef1fa
Marten Tacoma <Marten.Tacoma@nioz.nl>
2017-06-21 16:17:35 +0200

    Consistent closing of meta and link tags

--- app/npdc/template/head.tpl.php
+++ app/npdc/template/head.tpl.php
@@ -1,12 +1,12 @@
 <head>
 	<title><?=isset($view->title) && strlen($view->title) > 0 ? $view->title.' |' : '';?> <?=\npdc\config::$siteName?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0">
-	<meta name="author" content="Marten Tacoma, NIOZ, 2017">
-	<meta name="application-name" content="NPDC">
-	<meta name="page-version" content="<?=APP_VERSION?>">
-	<link rel="shortcut icon" type="image/x-icon" href="<?=BASE_URL?>/img/logo200.png">
-	<link rel="apple-touch-icon" href="<?=BASE_URL?>/img/logo200.png">
+	<meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0" />
+	<meta name="author" content="Marten Tacoma, NIOZ, 2017" />
+	<meta name="application-name" content="NPDC" />
+	<meta name="page-version" content="<?=APP_VERSION?>"/ >
+	<link rel="shortcut icon" type="image/x-icon" href="<?=BASE_URL?>/img/logo200.png" />
+	<link rel="apple-touch-icon" href="<?=BASE_URL?>/img/logo200.png" />
 	<?php
 	if(NPDC_DEV){
 		require 'css_js.php';
