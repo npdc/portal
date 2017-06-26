95768f39ad98cab188f9cfd99023a81201dce338
Marten Tacoma <Eupeodes@users.noreply.github.com>
2017-06-26 15:48:12 +0200

    Do not fire push in debug

--- app/npdc/lib/Push.php
+++ app/npdc/lib/Push.php
@@ -4,17 +4,19 @@ namespace npdc\lib;
 
 class Push {
 	public static function send($title, $url, $text=null){
-		$curl = 'https://maker.ifttt.com/trigger/'.\npdc\config::$ifttt['event'].'/with/key/'.\npdc\config::$ifttt['token'];
-		$ch = curl_init($curl);
-		$xml = 'value1='.$title.'&value2='.$url;
-		if(!empty($text)){
-			$xml .= '&value3='.$text;
+		if(!DEBUG){
+			$curl = 'https://maker.ifttt.com/trigger/'.\npdc\config::$ifttt['event'].'/with/key/'.\npdc\config::$ifttt['token'];
+			$ch = curl_init($curl);
+			$xml = 'value1='.$title.'&value2='.$url;
+			if(!empty($text)){
+				$xml .= '&value3='.$text;
+			}
+
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+
+			curl_exec($ch);
+			curl_close($ch);
 		}
-
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-
-		curl_exec($ch);
-		curl_close($ch);
 	}
 }
