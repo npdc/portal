b7d3cb1f55a7ff55b2ebd49f7d19d5b5d1e76984
Marten Tacoma <Eupeodes@users.noreply.github.com>
2017-09-05 17:39:36 +0200

    allow italics in titles of projects, datasets and publications, avoid these showing up in tweets

--- app/npdc/lib/Push.php
+++ app/npdc/lib/Push.php
@@ -7,9 +7,9 @@ class Push {
 		if(!NPDC_DEV){
 			$curl = 'https://maker.ifttt.com/trigger/'.\npdc\config::$ifttt['event'].'/with/key/'.\npdc\config::$ifttt['token'];
 			$ch = curl_init($curl);
-			$xml = 'value1='.$title.'&value2='.$url;
+			$xml = 'value1='.html_entity_decode(filter_var($title, FILTER_SANITIZE_STRING)).'&value2='.$url;
 			if(!empty($text)){
-				$xml .= '&value3='.$text;
+				$xml .= '&value3='.html_entity_decode(filter_var($text, FILTER_SANITIZE_STRING));
 			}
 
 			curl_setopt($ch, CURLOPT_POST, 1);
