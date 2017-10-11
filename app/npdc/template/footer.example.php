312fc2a6ef0b10f8f84e734ad82249bbf17085d2
Marten Tacoma <Eupeodes@users.noreply.github.com>
2017-10-11 09:59:00 +0200

    separate footer from page

---
+++ app/npdc/template/footer.example.php
@@ -0,0 +1,12 @@
+<div id="footer">
+	<div class="rechts">
+		The NPDC is hosted by us
+	</div>
+	<div class="links">
+		<a href="<?=BASE_URL?>/privacy">Privacy &amp; Cookies</a>
+		- 
+		<a href="<?=BASE_URL?>/terms">Terms of use</a>
+		- 
+		<a href="<?=BASE_URL?>/copyright">Copyright &amp; Credits</a>
+	</div>
+</div>
--- app/npdc/template/page.tpl.php
+++ app/npdc/template/page.tpl.php
@@ -85,20 +85,11 @@
 					echo '</div>';
 				}?>
 			</div>
-			<div id="footer">
-				<div class="rechts">
-					<span class="nobr">The Netherlands Polar Data Center</span> 
-					is <span class="nobr">hosted by <a href="http://www.nioz.nl"><img src="<?=BASE_URL?>/img/nioz.png" alt="NIOZ" class="nioz" /></a></span> 
-					<span class="nobr">at request of <a href="http://www.nwo.nl/en/research-and-results/programmes/Netherlands+Polar+Programme"><img src="<?=BASE_URL?>/img/nwo.png" alt="NWO" class="nwo" /></a></span>
-				</div>
-				<div class="links">
-					<a href="<?=BASE_URL?>/privacy">Privacy &amp; Cookies</a>
-					- 
-					<a href="<?=BASE_URL?>/terms">Terms of use</a>
-					- 
-					<a href="<?=BASE_URL?>/copyright">Copyright &amp; Credits</a>
-				</div>
-			</div>
+			<?php
+			if(file_exists(__DIR__.'/footer.tpl.php')){
+				require 'footer.tpl.php';
+			}
+			?>
 		</div>
 		<?php if(NPDC_DEV){
 			echo '<div class="debug bottom">Loading time: '.(microtime(true)-$start).'s<div>';
