59ffba4e80c6667261edebf0cc6af99da9417d89
Marten Tacoma <marten.tacoma@nioz.nl>
2018-02-13 12:56:30 +0100

    rearangement of frontblocks, removing twitter block

--- app/npdc/view/Front.php
+++ app/npdc/view/Front.php
@@ -43,19 +43,21 @@ class Front{
 		$model = new \npdc\model\Page();
 
 		$data = $model->getByUrl('home');
-		$this->mid = $data['content'];
-		$c = new \npdc\controller\Search($this->session, $this->args);
-		$v = new \npdc\view\Search($this->session, $this->args, $c);
-		$this->right = '<h3>Search</h3>'.$v->getForm();
+		$this->mid = '<div>'.$data['content'].'</div><div>';
 		
 		$newsModel = new \npdc\model\News();
 		$news = $newsModel->getLatest();
 		if(count($news) > 0){
-			$this->frontblocks['news'] = '<h4>'.$news[0]['title'].'</h4>'
+			$this->mid .= '<div><h4>'.$news[0]['title'].'</h4>'
 				. '<p class="info">Published: '.date('j F Y H:i', strtotime($news[0]['published'])).'</p>'
 				. '<p>'.$news[0]['content'].'</p>'
-				. '<a href="'.$news[0]['link'].'">More info</a>';
+				. '<a href="'.$news[0]['link'].'">More info</a></div>';
 		}
+
+		$c = new \npdc\controller\Search($this->session, $this->args);
+		$v = new \npdc\view\Search($this->session, $this->args, $c);
+		$this->mid .= '<div><div>'.$v->getForm().'</div></div></div>';
+
 		$list = [];
 		foreach(['project', 'dataset', 'publication'] as $type){
 			$modelName = 'npdc\\model\\'.ucfirst($type);
@@ -78,7 +80,7 @@ class Front{
 		$list = array_combine($keys, $list);
 		krsort($list);
 		$list = array_slice($list, 0, \npdc\config::$showNew);
-		$block = '<h4>Recently added or updated</h4>';
+		$block = '<h3>Recently added or updated</h3>';
 		foreach($list as $item){
 			$block .= '<hr/>';
 			if(array_key_exists('project_id', $item)){
@@ -91,12 +93,7 @@ class Front{
 			$block .= '<a href="'.BASE_URL.'/'.strtolower($type).'/'.$item[strtolower($type).'_id'].'">'.trim($item['title']).'</a> <span class="info"><span class="type">'.$type.'</span> <span class="time">'.date('j F Y H:i', strtotime($item['published'])).'</span></span>';
 		}
 		
-		$this->frontblocks['update'] = $block;
-		
-		if(!empty(\npdc\config::$social['twitter'])){
-			$this->frontblocks['twitter'] = '<a class="twitter-timeline" data-height="calc(100% - 20px)" href="https://twitter.com/'.\npdc\config::$social['twitter'].'">Tweets by '.\npdc\config::$social['twitter'].'</a> '
-			. '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
-			;
-		}
+		//$this->frontblocks['update'] = $block;
+		$this->right = $block;
 	}	
 }
