dc97ce0526af2cfde98853c43909f56ea18efd14
Marten Tacoma <marten.tacoma@nioz.nl>
2018-11-15 11:39:10 +0100

    include timestamp

--- private/npdc/sql/mysql_update_20181115.sql
+++ private/npdc/sql/mysql_update_20181115.sql
@@ -1,15 +1,13 @@
-CREATE TABLE contact (
-	contact_id INT NOT NULL AUTO_INCREMENT,
-	receiver TEXT NOT NULL,
-	sender_mail TEXT NOT NULL,
-	sender_name TEXT NOT NULL,
-	subject TEXT NULL,
-	`text` TEXT NOT NULL,
-	country TEXT NULL COMMENT 'this should be empty, is the anti-spam field',
-	ip varchar(100) NULL,
-  browser TEXT NULL,
-	CONSTRAINT contact_PK PRIMARY KEY (contact_id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
+  `receiver` text NOT NULL,
+  `sender_mail` text NOT NULL,
+  `sender_name` text NOT NULL,
+  `subject` text,
+  `text` text NOT NULL,
+  `country` text COMMENT 'this should be empty, is the anti-spam field',
+  `ip` varchar(100) DEFAULT NULL,
+  `browser` text,
+  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4
