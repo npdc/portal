6c222b0a5a10c176b6aff3a1e173a720c8b6e7fe
Marten Tacoma <marten.tacoma@nioz.nl>
2018-11-16 08:18:01 +0100

    Remove db name from mysql_update files

--- private/npdc/sql/mysql_update_20180705.sql
+++ private/npdc/sql/mysql_update_20180705.sql
@@ -1,3 +1,3 @@
-ALTER TABLE c2_npdc.dataset ADD uuid VARCHARACTER(36) NULL;
-ALTER TABLE c2_npdc.project ADD uuid VARCHARACTER(36) NULL;
-ALTER TABLE c2_npdc.publication ADD uuid VARCHARACTER(36) NULL;
+ALTER TABLE npp_themedataset ADD uuid VARCHARACTER(36) NULL;
+ALTER TABLE npp_themeproject ADD uuid VARCHARACTER(36) NULL;
+ALTER TABLE npp_themepublication ADD uuid VARCHARACTER(36) NULL;
--- private/npdc/sql/mysql_update_20181115_2.sql
+++ private/npdc/sql/mysql_update_20181115_2.sql
@@ -5,8 +5,8 @@ CREATE TABLE `npp_theme` (
   PRIMARY KEY (`npp_theme_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE c2_npdc.program ADD sort SMALLINT NULL;
-ALTER TABLE c2_npdc.project MODIFY COLUMN nwo_project_id varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL;
-ALTER TABLE c2_npdc.project ADD npp_theme_id integer NULL;
-ALTER TABLE c2_npdc.project ADD CONSTRAINT project_x_npp_theme_fk FOREIGN KEY (npp_theme_id) REFERENCES c2_npdc.npp_theme(npp_theme_id);
+ALTER TABLE npp_themeprogram ADD sort SMALLINT NULL;
+ALTER TABLE npp_themeproject MODIFY COLUMN nwo_project_id varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL;
+ALTER TABLE npp_themeproject ADD npp_theme_id integer NULL;
+ALTER TABLE npp_themeproject ADD CONSTRAINT project_x_npp_theme_fk FOREIGN KEY (npp_theme_id) REFERENCES npp_themenpp_theme(npp_theme_id);
 
