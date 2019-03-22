72288819e75899c7a3f04826f6f67ce2f4450844
Marten Tacoma <marten.tacoma@nioz.nl>
2019-03-22 15:15:57 +0100

    Include sass into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,44 @@ const gulp = require('gulp')
 	, sourcemaps = require('gulp-sourcemaps')
 	, uglify = require('gulp-uglify')
 	, rename = require('gulp-rename')
-	, filter = require('gulp-filter');
+	, filter = require('gulp-filter')
+	, sass = require('gulp-sass');
 
-function minifyJS(sourceFiles, destinationFolder, filenameRoot) {
-	return gulp.src(sourceFiles)
-		.pipe(sourcemaps.init())
-		.pipe(concat(filenameRoot + '.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(destinationFolder))
-		.pipe(filter('**/*.js'))
+function minifyJS(file) {
+	return gulp.src('private/npdc/javascript/' + file, {sourcemaps: true})
+		.pipe(concat(file + '.js'))
 		.pipe(uglify())
 		.pipe(rename({ extname: '.min.js' }))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(destinationFolder));
+		.pipe(gulp.dest('web/js', {sourcemaps: '.'}));
 }
 
-gulp.task('js-editor', function(){
-	return minifyJS('private/npdc/javascript/editor/*', 'web/js', 'editor');
+function css2scss(){
+	return gulp.src(['private/npdc/scss/*.scss', '!private/npdc/scss/[a-z]_*.scss'], {sourcemaps: true})
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(rename({extname: '.min.css'}))
+		.pipe(gulp.dest('web/css', {sourcemaps: '.'}));
+}
+
+gulp.task('css', function(){
+	return css2scss();
 });
 
-gulp.task('js-main', function(){
-	return minifyJS('private/npdc/javascript/main/*', 'web/js', 'npdc');
+gulp.task('js-editor', function(){
+	return minifyJS('editor');
+});
+
+gulp.task('js-npdc', function(){
+	return minifyJS('npdc');
+});
+
+gulp.task('watch', function(){
+	gulp.watch('private/npdc/javascript/editor/*.js', function(){
+		return minifyJS('editor');
+	});
+	gulp.watch('private/npdc/javascript/main/*.js', function(){
+		return minifyJS('npdc');
+	});
+	gulp.watch('private/npdc/scss/*.scss', function(){
+		return css2scss();
+	});
 });
